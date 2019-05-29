f5422c88cfaf040d3605061cd4efb3174aa05f06
AaronRockele <aa.rockele@gmail.com>
2019-05-29 15:02:44 +0200

    finished

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,8 +1,22 @@
+window.onload = function() {
+    document.getElementById("doge").innerHTML = "woof!"
+}
+
 function overText()
 {
-    document.getElementById("doge").innerHTML = "woef!";
+    document.getElementById("doge").innerHTML = "woof woof!";
 }
 function oldText()
 {
-    document.getElementById("doge").innerHTML = "doggy";
+    document.getElementById("doge").innerHTML = "doggo";
+}
+
+function picture()
+{
+    if (document.getElementById("pic1").style.display === "block"){
+        document.getElementById("pic1").style.display = "none";
+    }
+    else {
+        document.getElementById("pic1").style.display = "block";
+    }
 }
